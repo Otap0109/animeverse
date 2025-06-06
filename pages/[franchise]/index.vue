7a5ee89cfb1464b443d647500625f177fa57c5a8
Otap0109 <ostapprusik@gmail.com>
2025-06-06 20:43:03 +0200

    init

--- pages/[franchise]/index.vue
+++ pages/[franchise]/index.vue
@@ -9,7 +9,7 @@ const franchise = computed(() => route.params.franchise);
 const characters = ref([]);
 
 try {
-  const response = await axios.get(`http://127.0.0.1:8000/api/characters/`, {
+  const response = await axios.get(`http://127.0.0.1:8000/api/character/`, {
     params: { 'anime__slug': franchise.value }, // Filter by anime slug
   });
   characters.value = response.data.results;
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@ import axios from 'axios';
 const data = ref([]);
 
 try {
-  const response = await axios.get('http://127.0.0.1:8000/api/anime/');
+  const response = await axios.get('http://127.0.0.1:8000/api/franchise/');
   data.value = response.data.results;
 } catch (error) {
   console.error(error);
